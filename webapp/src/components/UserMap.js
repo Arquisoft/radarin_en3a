570f9283b36d99fde4daa6bf81a9c9844cc71c1c
raulng9 <salprez4@gmail.com>
2021-03-29 13:18:54 +0200

    Added profile pic viewing for owner and friend PODs

--- webapp/src/components/UserMap.js
+++ webapp/src/components/UserMap.js
@@ -7,6 +7,7 @@ function UserMap(props) {
 
     // Map creation after the mounting of the div#map-panel element on the DOM
     useEffect(() => {
+
         var mymap = L.map('map-panel').setView([51.505, -0.09], 13);
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
--- webapp/src/components/Welcome.js
+++ webapp/src/components/Welcome.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {List, Value, useWebId, useLDflexList} from '@solid/react';
+import {List, Value, useWebId, useLDflexList, Image, useLDflex} from '@solid/react';
 import "../css/Welcome.css";
-
+import defaultProfilePic from '../assets/default_profile_pic.svg';
 function Welcome(props) {
+    //const {default: data} = require('@solid/query-ldflex');
     const webId = useWebId();
+
     const friends = useLDflexList('user.friends');
-  return (<div className="logged-in-panel">
+    const profPicProxy = useLDflex(`[${webId}].vcard_hasPhoto`);
+    const profilePicSrc = profPicProxy[0];
+
+    return (
+        <div className="logged-in-panel">
             <h1>Hello there!</h1>
+             <Image src={profilePicSrc} defaultSrc={defaultProfilePic} className="owner-profile-pic"/>
             <h2>Logged as <Value src="user.name"/></h2>
             <span>Your webID is: {webId}</span>
             <h3 className="title-number-friends">You have {friends.length} friends added:</h3>
@@ -15,10 +22,13 @@ function Welcome(props) {
               <li key={friend} onClick={(e) => window.location.href=friend}>
                   <button >
                       <Value src={`[${friend}].name`}>{`${friend}`}</Value>
+                      <br/>
+                      <Image className="friend-profile-pic" src={`[${friend}].vcard_hasPhoto`} defaultSrc={defaultProfilePic}>{`${friend}`}</Image>
                   </button>
               </li>}
       </List>
-        </div>);
+        </div>
+    );
 }
 
 export default Welcome;
