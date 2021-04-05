fe59c1d35b361f9bff2e31d846d2a2b1644d36f3
Sergio <uo270411@uniovi.es>
2021-04-05 23:36:53 +0200

    Some unit tests for components

--- webapp/src/tests/InitialWelcome.test.js
+++ webapp/src/tests/InitialWelcome.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, fireEvent } from "@testing-library/react";
-import InitialWelcome from "../components/InitialWelcome";
+import InitialWelcome from "./../components/InitialWelcome";
 
 test('check that everything is rendering propertly', async () => {
   const { getByText } = render(<InitialWelcome/>);
