ba87e374a90e3d5b7fcce5f60be5d5ad5d14ad2d
iree <uo269570@uniovi.es>
2021-04-06 10:08:12 +0200

    Remove notifications, leave the map only

--- AppMovile/Screens/HomeScreen.js
+++ AppMovile/Screens/HomeScreen.js
@@ -2,24 +2,11 @@ import React, { useState, useEffect, useRef } from 'react';
 import MapView from 'react-native-maps';
 import {Text, TouchableOpacity, View, StyleSheet, Dimensions} from 'react-native';
 import * as Location from 'expo-location';
-import * as Notifications from 'expo-notifications';
-import Constants from 'expo-constants';
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
-});
 
 export default function Home() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
-  const [expoPushToken, setExpoPushToken] = useState('');
-  const [notification, setNotification] = useState(false);
-  const notificationListener = useRef();
-  const responseListener = useRef();
 
   useEffect(() => {
     (async () => {
@@ -33,23 +20,6 @@ export default function Home() {
       setLocation(location);
       
     })();
-
-    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
-
-    // This listener is fired whenever a notification is received while the app is foregrounded
-    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-      setNotification(notification);
-    });
-
-    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-      console.log(response);
-    });
-
-    return () => {
-      Notifications.removeNotificationSubscription(notificationListener.current);
-      Notifications.removeNotificationSubscription(responseListener.current);
-    };
   }, []);
 
   let text = 'Waiting..';
@@ -81,64 +51,11 @@ export default function Home() {
       <TouchableOpacity
               style={{backgroundColor:"#03a9f4", padding: 10, width: 150, borderRadius:30,
               marginHorizontal:10, borderWidth:1, borderColor:'#03a9f4'}}
-              onPress={async () => {
-                await sendPushNotification(expoPushToken,latitude,longitude);
-              }} >
+               >
               <Text style={{textAlign:'center', color:"#000000", fontSize:18}}>Save</Text>
       </TouchableOpacity>
     </View>
   );
-}
-
-async function sendPushNotification(expoPushToken, lat, long) {
-  const message = {
-    to: expoPushToken,
-    sound: 'default',
-    title: 'Your location is saved',
-    body: 'lat: ' + lat + ' lon: ' + long,
-    data: {  },
-  };
-
-  await fetch('https://exp.host/--/api/v2/push/send', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Accept-encoding': 'gzip, deflate',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(message),
-  });
-}
-
-async function registerForPushNotificationsAsync() {
-  let token;
-  if (Constants.isDevice) {
-    const { status: existingStatus } = await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== 'granted') {
-      alert('Failed to get push token for push notification!');
-      return;
-    }
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-    console.log(token);
-  } else {
-    alert('Must use physical device for Push Notifications');
-  }
-
-  if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
-  }
-
-  return token;
 }
 
 const styles = StyleSheet.create({
