5494a0325f0a649da64e6525f2f647e4c20c69a6
Sergio <uo270411@uniovi.es>
2021-04-06 20:10:54 +0200

    Authenticated navigation translation test

--- webapp/src/tests/AuthNavTranslation.test.js
+++ webapp/src/tests/AuthNavTranslation.test.js
@@ -1,30 +1,27 @@
 import React from 'react'
 import { render, fireEvent } from "@testing-library/react";
-import Navigation from "../components/fragments/Navigation";
+import NavAuthenticated from "../components/fragments/NavAuthenticated";
 import i18n from '../i18n'
 import { I18nextProvider } from 'react-i18next'
 
 test('check that language is changing', async () => {
   const c = render(
     <I18nextProvider i18n={i18n}> // actually give translation to your component
-       <Navigation />
+       <NavAuthenticated />
     </I18nextProvider>
   );
-  expect(c.getAllByText(i18n.getDataByLanguage('en').translation.navBarAbout)).toBeDefined(); 
-  expect(c.getAllByText(i18n.getDataByLanguage('en').translation.navBarLogIn)).toBeDefined(); 
-  expect(c.getAllByText(i18n.getDataByLanguage('en').translation.navBarSignUp)).toBeDefined(); 
+  expect(c.getAllByText(i18n.getDataByLanguage('en').translation.navBarMap)).toBeDefined(); 
+  expect(c.getAllByText(i18n.getDataByLanguage('en').translation.navBarProfile)).toBeDefined(); 
   const dropdown = c.getByText("Language");
   fireEvent.click(dropdown);
   const changeLanguage = c.getByText("Spanish");
   fireEvent.click(changeLanguage);
-  expect(c.getAllByText(i18n.getDataByLanguage('es').translation.navBarAbout)).toBeDefined(); 
-  expect(c.getAllByText(i18n.getDataByLanguage('es').translation.navBarLogIn)).toBeDefined(); 
-  expect(c.getAllByText(i18n.getDataByLanguage('es').translation.navBarSignUp)).toBeDefined(); 
+  expect(c.getAllByText(i18n.getDataByLanguage('es').translation.navBarMap)).toBeDefined(); 
+  expect(c.getAllByText(i18n.getDataByLanguage('es').translation.navBarProfile)).toBeDefined(); 
   const Spanishdropdown = c.getByText("Idioma");
   fireEvent.click(Spanishdropdown);
   const changeToEnglish = c.getByText("Inglés");
   fireEvent.click(changeToEnglish);
-  expect(c.getAllByText(i18n.getDataByLanguage('en').translation.navBarAbout)).toBeDefined(); 
-  expect(c.getAllByText(i18n.getDataByLanguage('en').translation.navBarLogIn)).toBeDefined(); 
-  expect(c.getAllByText(i18n.getDataByLanguage('en').translation.navBarSignUp)).toBeDefined(); 
+  expect(c.getAllByText(i18n.getDataByLanguage('en').translation.navBarMap)).toBeDefined(); 
+  expect(c.getAllByText(i18n.getDataByLanguage('en').translation.navBarProfile)).toBeDefined(); 
 });
