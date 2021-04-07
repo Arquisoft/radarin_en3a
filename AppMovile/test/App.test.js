32dc1bf6fcaadb72bdf1147be80f8642710666b7
Armando Campa <uo257850@uniovi.es>
2021-04-07 20:15:25 +0200

    trying folder structure for testing

---
+++ AppMovile/test/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render, fireEvent } from "@testing-library/react";
+import App from "../App";
+
+test('check that everything is rendering propertly', async() => {
+    const { getByText, getByLabelText } = render( < EmailForm / > );
+    expect(getByText(/Radarin/i)).toBeInTheDocument();
+    //screen.getByLabelText("Register").toBeInTheDocument();
+});
