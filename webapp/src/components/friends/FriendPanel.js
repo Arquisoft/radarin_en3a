376da605649c79d6514e7df6852151aeeb98e482
raulng9 <salprez4@gmail.com>
2021-04-13 19:47:34 +0200

    Changed Friend Panel to retrieve name correctly

--- webapp/src/components/friends/FriendPanel.js
+++ webapp/src/components/friends/FriendPanel.js
@@ -1,14 +1,18 @@
-import {Image, useLDflex} from "@solid/react";
+import {Name, Link, Image, useLDflex} from "@solid/react";
 import defaultProfilePic from '../../assets/default_profile_pic.svg';
+import {useTranslation} from "react-i18next";
 
 function FriendPanel(props){
-    const profPicProxy = useLDflex(`[${props.nombre}].vcard_hasPhoto`);
+    const profPicProxy = useLDflex(`[${props.name}].vcard_hasPhoto`);
     const profilePicSrc = profPicProxy[0];
+    const { t } = useTranslation();
 
     return(
         <div>
-            <Image className="friend-profile-pic" src={profilePicSrc} defaultSrc={defaultProfilePic}/>
+            <Image className="friend-profile-pic" src={profilePicSrc} defaultSrc={defaultProfilePic}>{ props.name }</Image>
+            <Name src={props.name}>{props.name}</Name>
             <br/>
+            <Link href={props.name} className="link" datatype="link">{t('Profile')}</Link>
         </div>);
 }
 
