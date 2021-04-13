79675ee295a123eb27cd2299bcb6dd8954559678
Sergio <uo270411@uniovi.es>
2021-04-13 18:58:01 +0200

    Unitary tests updated

--- webapp/src/tests/FriendsPanel.test.js
+++ webapp/src/tests/FriendsPanel.test.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { render, fireEvent } from "@testing-library/react";
 import FriendPanel from "./../components/friends/FriendPanel";
+import i18n from '../i18n'
+import { I18nextProvider } from 'react-i18next'
 
 test('check that everything is rendering propertly', async () => {
-  const { getByText, getAllByRole } = render(<FriendPanel/>);
-  const image = getAllByRole('img')
-  expect(image).toHaveLength(1);
+  const c = render(
+    <I18nextProvider i18n={i18n}> // actually give translation to your component
+       <FriendPanel />
+    </I18nextProvider>
+  );
+  expect(c.getAllByText(i18n.getDataByLanguage('en').translation.Profile)).toBeDefined();
 });
