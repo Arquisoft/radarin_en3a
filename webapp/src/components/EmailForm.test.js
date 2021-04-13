31ebd8e37cdb2a9a3908503cbe37bcd07e54f353
UO269509 <uo269509@uniovi.es>
2021-04-13 12:24:14 +0200

    Deleting old components and tests and adding older components

--- webapp/src/components/EmailForm.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { render } from "@testing-library/react";
-import EmailForm from "./EmailForm";
-
-test('check that everything is rendering propertly', async () => {
-  const { getByText } = render(<EmailForm/>);
-  expect(getByText("Submit")).toBeInTheDocument();
-});
--- webapp/src/components/RegisterForm.js
+++ webapp/src/components/RegisterForm.js
@@ -10,7 +10,6 @@ function RegisterForm(props) {
         <Card className="register-card" bg="dark" style={{  }}>
             <Card.Body>
                 <Card.Title className="register-title">{t('registerFormTitle')}</Card.Title>
-                <Button className="register-btn" variant="primary" href="https://inrupt.net/register" block>Inrupt</Button>
                 <Button className="register-btn" variant="primary" href="https://solidcommunity.net/register" block>Solid Community</Button>
             </Card.Body>
         </Card>
