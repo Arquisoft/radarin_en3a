ad65cf9cb8f08cce7167ddee2b9bd5d026879d78
iree <uo269570@uniovi.es>
2021-04-14 17:22:30 +0200

    try fixing errors with manage users

--- webapp/src/components/admin/ManageUsers.js
+++ webapp/src/components/admin/ManageUsers.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, {useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import '../../css/Navigation.css';
 import Button from "react-bootstrap/Button";
@@ -7,39 +7,43 @@ import { getUsers, removeUser } from "../../api/api.js"
 
 function ManageUsers () {
 
-    let usersList = null;
+    const [usersList, setUserList] = useState([]);
 
     const { t } = useTranslation();
     
     async function ReturnUsers() {
-        usersList = await getUsers();
+        let lista = await getUsers();
+        return lista;
     }
 
     async function DeleteUser(user) {
         removeUser(user.webId);
-        ReturnUsers();
+        setUserList(ReturnUsers());
     }
 
     useEffect(() => {
-        ReturnUsers();
-    });
+        setUserList(ReturnUsers());
+    }, [usersList]);
   
-    return(<div>
-        <h2>{t('AdminList')}</h2>
-        {usersList.map((user) => 
-            {return <ListGroup horizontal style={{ margin: "20px" }}>
-                        <ListGroup.Item style={{ minWidth: "500px", textAlign: "center" }}>
-                            {user.webId}
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Button data-testid={user.webId} onClick={(user)=>{
-                                DeleteUser(user);
-                            }}>{t('AdminDelete')}</Button>
-                        </ListGroup.Item>
-                    </ListGroup>      
-            }
-        )}
-    </div>)
+    return(
+        <div>
+            <h2>{t('AdminList')}</h2>
+            {usersList.map((user) => 
+                {return <ListGroup horizontal style={{ margin: "20px" }}>
+                            <ListGroup.Item style={{ minWidth: "500px", textAlign: "center" }}>
+                                {user.webId}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button data-testid={user.webId} onClick={(user)=>{
+                                    DeleteUser(user);
+                                }}>{t('AdminDelete')}</Button>
+                            </ListGroup.Item>
+                        </ListGroup>      
+                }
+            )}
+
+        </div>
+    );
 }
 
 export default ManageUsers;
