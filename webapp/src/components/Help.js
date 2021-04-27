38b39f0150cb97ed419e968da5cfb3105b84eff5
Armando <uo257850@uniovi.es>
2021-04-27 19:51:52 +0200

    Update Help.js

--- webapp/src/components/Help.js
+++ webapp/src/components/Help.js
@@ -6,7 +6,12 @@ import { useTranslation } from 'react-i18next';
 function Help(props) {
     return (
         <div className="Hello trying">
-           
+            <h1>Languaje</h1>
+            <h1>How to log in and out</h1>
+            <h1>How to save a location</h1>
+            <h1>How to see your locations</h1>
+            <h1>How to see your friends</h1>
+            <h1>Notifications</h1>
         </div>
     );
 }
