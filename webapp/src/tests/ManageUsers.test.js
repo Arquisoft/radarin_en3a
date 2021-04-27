18fa26e9b6a0631f7c3dfde7e655eae7b1cdb614
Sergio <uo270411@uniovi.es>
2021-04-27 23:59:43 +0200

    Unitary tests updated and improved

--- webapp/src/tests/ManageUsers.test.js
+++ webapp/src/tests/ManageUsers.test.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { render } from "@testing-library/react";
-import FriendsView from "./../components/friends/FriendsView";
+import ManageUsers from "../components/admin/ManageUsers";
 import i18n from '../i18n'
 import { I18nextProvider } from 'react-i18next'
-
 
 test('check that everything is rendering propertly', async () => {
   const c = render(
     <I18nextProvider i18n={i18n}> // actually give translation to your component
-       <FriendsView />
+       <ManageUsers /> 
     </I18nextProvider>
   );
-  expect(c.getAllByText(i18n.getDataByLanguage('en').translation.FriendList + "(total 0)")).toBeDefined();
-  //expect(getByText("Friend list (total 0)")).toBeDefined();
-}); 
+  expect(c.getAllByText(i18n.getDataByLanguage('en').translation.AdminList)).toBeDefined();
+});
