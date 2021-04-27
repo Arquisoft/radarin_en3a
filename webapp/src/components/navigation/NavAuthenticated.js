a1ad19fd85a87cc7c1640178cd3bb2657ee36f9a
Armando Campa <uo257850@uniovi.es>
2021-04-27 10:01:00 +0200

    package updates

--- webapp/src/components/navigation/NavAuthenticated.js
+++ webapp/src/components/navigation/NavAuthenticated.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import MapView from "../map/MapView";
 import LocationsView from "../locations/LocationsView";
@@ -13,14 +13,14 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
-import {getDefaultSession, logout} from "@inrupt/solid-client-authn-browser";
+import { getDefaultSession, logout } from "@inrupt/solid-client-authn-browser";
 import { useTranslation } from 'react-i18next';
-import {CombinedDataProvider, Text, useSession} from "@inrupt/solid-ui-react";
+import { CombinedDataProvider, Text, useSession } from "@inrupt/solid-ui-react";
 import ManageUsers from '../admin/ManageUsers';
 import { addLocation, addUser, getUserByWebId } from '../../api/api.js';
-import '../Help.js';
+import '../help/Help.js';
 
-function NavAuthenticated(){
+function NavAuthenticated() {
 
     const { session } = useSession();
     const { t, i18n } = useTranslation();
@@ -33,12 +33,12 @@ function NavAuthenticated(){
         navigator.geolocation.getCurrentPosition(async function (position) {
             console.log("Esto es lo que le estamos añadiendo al usuario: " + webId + " localización longitud: " + position.coords.longitude + " latitud: " + position.coords.latitude);
             let usuario = await getUserByWebId(webId);
-            if(usuario == null){
-                usuario = await addUser(webId, position.coords.longitude, position.coords.latitude );
+            if (usuario == null) {
+                usuario = await addUser(webId, position.coords.longitude, position.coords.latitude);
                 console.log(usuario);
                 setRole(usuario.role);
-            }else{
-                await addLocation(usuario._id, position.coords.longitude, position.coords.latitude );
+            } else {
+                await addLocation(usuario._id, position.coords.longitude, position.coords.latitude);
                 setRole(usuario.role);
             }
         });
@@ -52,47 +52,47 @@ function NavAuthenticated(){
     };
 
 
-        return (
-            <div>
-                <Navbar collapseOnSelect navbar="dark" bg="primary" expand="lg" fixed="top">
-                    <Navbar.Brand href="#">
-                        <img
-                            src={logo}
-                            width="40"
-                            height="40"
-                            className="d-inline-block align-top"
-                            alt="Radarin logo"
-                        />
-                        <p className="radarin-title">Radarin</p>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <DropdownButton id="dropdown-item-button" style={{margin: "16px"}} variant="secondary" title={t('navBarLanguage')}>
-                            <Dropdown.Item as="button" onClick={() => changeLanguage('en')}>{t('navBarLanguageEn')}</Dropdown.Item>
-                            <Dropdown.Item as="button" onClick={() => changeLanguage('es')}>{t('navBarLanguageEs')}</Dropdown.Item>
-                        </DropdownButton>
-                
-                <Nav className="mr-auto">
-                    {(() => {
-                        if (role != null && role === "Admin") {
-                            return (
-                                <Nav.Link className="mt-1 mr-2" href="#/manageUsers">{t('AdminList')}</Nav.Link>
-                            );
-                        }
-                    })()}
-                    <Nav.Link  id="profile-nav-link" className="mt-1 mr-2" href="#/profile">{t('navBarProfile')}</Nav.Link>
-                    <Nav.Link  className="mt-1 mr-2" href="#/map">{t('navBarMap')}</Nav.Link>
-                    <Nav.Link  className="mt-1 mr-2" href="#/locations">{t('navBarLocations')}</Nav.Link>
-                    <Nav.Link  className="mt-1 mr-2" href="#/friends">{t('navBarFriends')}</Nav.Link>
-                    <Nav.Link  className="mt-1 mr-2" href="#/help">{t('navBarHelp')}</Nav.Link>
-                    <Button className="log-out-btn" onClick={(e) => handleLogout(e)}>{t('navBarLogOut')}</Button>
-                </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-                <CombinedDataProvider
-                    datasetUrl={session.info.webId}
-                    thingUrl={session.info.webId}
-                >
+    return (
+        <div>
+            <Navbar collapseOnSelect navbar="dark" bg="primary" expand="lg" fixed="top">
+                <Navbar.Brand href="#">
+                    <img
+                        src={logo}
+                        width="40"
+                        height="40"
+                        className="d-inline-block align-top"
+                        alt="Radarin logo"
+                    />
+                    <p className="radarin-title">Radarin</p>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <DropdownButton id="dropdown-item-button" style={{ margin: "16px" }} variant="secondary" title={t('navBarLanguage')}>
+                        <Dropdown.Item as="button" onClick={() => changeLanguage('en')}>{t('navBarLanguageEn')}</Dropdown.Item>
+                        <Dropdown.Item as="button" onClick={() => changeLanguage('es')}>{t('navBarLanguageEs')}</Dropdown.Item>
+                    </DropdownButton>
+
+                    <Nav className="mr-auto">
+                        {(() => {
+                            if (role != null && role === "Admin") {
+                                return (
+                                    <Nav.Link className="mt-1 mr-2" href="#/manageUsers">{t('AdminList')}</Nav.Link>
+                                );
+                            }
+                        })()}
+                        <Nav.Link id="profile-nav-link" className="mt-1 mr-2" href="#/profile">{t('navBarProfile')}</Nav.Link>
+                        <Nav.Link className="mt-1 mr-2" href="#/map">{t('navBarMap')}</Nav.Link>
+                        <Nav.Link className="mt-1 mr-2" href="#/locations">{t('navBarLocations')}</Nav.Link>
+                        <Nav.Link className="mt-1 mr-2" href="#/friends">{t('navBarFriends')}</Nav.Link>
+                        <Nav.Link className="mt-1 mr-2" href="#/help">{t('navBarHelp')}</Nav.Link>
+                        <Button className="log-out-btn" onClick={(e) => handleLogout(e)}>{t('navBarLogOut')}</Button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <CombinedDataProvider
+                datasetUrl={session.info.webId}
+                thingUrl={session.info.webId}
+            >
 
                 <div>
                     <div className="logged-in-msg-panel">
@@ -103,18 +103,19 @@ function NavAuthenticated(){
                                 "http://xmlns.com/foaf/0.1/name",]}
                         />
                     </div>
-                    <div id="container" style={{ backgroundColor: "black"}}>
-                        <Route exact path="/profile" component={WelcomeAuth}/>
-                        <Route exact path="/map" component={MapView}/>
-                        <Route exact path="/locations" component={LocationsView}/>
-                        <Route exact path="/friends" component={FriendsView}/>
-                        <Route exact path="/manageUsers" component={ManageUsers}/>                        
-                        <Route exact path="/help" component={Help}/>
+                    <div id="container" style={{ backgroundColor: "black" }}>
+                        <Route exact path="/profile" component={WelcomeAuth} />
+                        <Route exact path="/map" component={MapView} />
+                        <Route exact path="/locations" component={LocationsView} />
+                        <Route exact path="/friends" component={FriendsView} />
+                        <Route exact path="/manageUsers" component={ManageUsers} />
+                        <Route exact path="/help" component={Help} />
                         <Redirect path="/" exact to="/profile" />
                     </div>
                 </div>
             </CombinedDataProvider>
         </div>
-    )}
+    )
+}
 
 export default NavAuthenticated;
