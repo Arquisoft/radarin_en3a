6cf0929e4f54f31531294aa407c1fe1708abe2a8
iree <uo269570@uniovi.es>
2021-04-27 19:03:30 +0200

    fixded infinite loop

--- webapp/src/components/admin/RegUserComponent.js
+++ webapp/src/components/admin/RegUserComponent.js
@@ -1,24 +1,28 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import {removeUser} from "../../api/api";
+import {removeUser, getUsers} from "../../api/api";
 import {useTranslation} from "react-i18next";
+import $ from "jquery";
 
 
 function RegUserComponent(props) {
 
     const { t } = useTranslation();
 
-    const usersList = props.usersList;
+    let usersList = props.usersList;
 
     async function DeleteUser(user) {
         console.log(user);
         await removeUser(user);
+        usersList = await getUsers();
+        $('li').remove("#"+user);
+        //window.location.reload(false);
     }
 
     if(usersList.length === 0){return <h3>{t('MessageAdmin')}</h3>}
     return usersList.map(function(user,index){
         return (
-        <li key={index} className="registered-user-list-item">
+        <li key={index} id={user.webId} className="registered-user-list-item">
             {user.webId}
             <Button className="btn-danger" data-testid={user.webId} onClick={
                 () => DeleteUser(user.webId)
