558e0d740774f5a81f6865c25cfe60d7fcf0e064
UO225811 <UO225811@uniovi.es>
2021-04-28 13:36:10 +0200

    pushing again e2e test with the node error

--- webapp/e2e/steps/login-form.steps.js
+++ webapp/e2e/steps/login-form.steps.js
@@ -1,31 +1,46 @@
 const {defineFeature, loadFeature}=require('jest-cucumber');
 const feature = loadFeature('./features/login-form.feature');
+const expect = require('expect-puppeteer');
+const puppeteer = require('puppeteer');
 
 defineFeature(feature, test => {
-
-    beforeEach(async () => {
-        await global.page.goto('http://localhost:3000/#', {timeout: 60000});
-    });
     
+    beforeEach(async () => {
+        await global.page.goto('http://localhost:3000/#/');
+    });
+
+    // const browser = await puppeteer.launch({
+    //     headless: false,
+    //     defaultViewport: null
+    // });
+
     test('The user is already registered with a pod and logs in Radarin', ({given,when,then}) => {
     
+        let username;
+        let pass;
+
         given('An already registered user with a pod in radarin', () => {
-            username = "radarintest";
+             username = "radarintest";
+             pass = "Radarintest1.";
         });
     
         when('I fill in the user name field and press the login button', async () => {
-            await page.waitForSelector('input[name="userName"]');
-            await expect(page).toFill('input[name="userName"]', username);            
+            await expect(page).toMatch('Radarin');
+            await expect(page).toFill('input[name="userName"]', username);
             
-            await page.waitForSelector('button[text="Log in"]');
             await expect(page).toClick('button', { text: 'Log in' }); 
             
             await page.goto('https://solidcommunity.net/login');
-            await page.toMatch('Login');
+            await expect(page).toMatch('Login');
+            await expect(page).toFill('input[name="username"]', username);
+            await expect(page).toFill('input[name="password"]', pass);
+
+            await expect(page).toClick('button', {text: 'Log In'});
+            await page.goto('http://localhost:3000/#/profile');
         });
     
         then('I should enter in my radarin profile', async () => {
-            await expect(page).toMatch('http://localhost:3000/#/profile');
+            await expect(page).toMatch('Welcome!');
         });
     });
 });
