9b08aad6bff6170f376a45945e6f80800cc7e741
iree <uo269570@uniovi.es>
2021-05-01 13:43:22 +0200

    solve error test

--- webapp/src/tests/Marker.test.js
+++ webapp/src/tests/Marker.test.js
@@ -3,6 +3,6 @@ import { render } from "@testing-library/react";
 import MarkerGenerator from "./../components/map/MarkerGenerator";
 
 test("check that everything is rendering propertly", async () => {
-  const  getByText = render(<MarkerGenerator/>);
+  const { getByText } = render(<MarkerGenerator/>);
   expect(getByText("No locations currently saved on your POD")).toBeInTheDocument();
 });  
