d550090eae916cbb43c702b262fe42e39d22d3d9
UO225811 <UO225811@uniovi.es>
2021-05-02 23:12:55 +0200

    Solving merge conflicts

--- webapp/e2e/steps/login-form.steps.js
+++ webapp/e2e/steps/login-form.steps.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const {defineFeature, loadFeature}=require('jest-cucumber');
 const feature = loadFeature('./features/login-form.feature');
 const {setDefaultOptions} = require('expect-puppeteer');
@@ -18,25 +17,6 @@ defineFeature(feature, test => {
     });
 
     test('The user is already registered with a pod and logs in Radarin', ({given,when,then}) => {
-=======
-const {defineFeature, loadFeature}=require("jest-cucumber");
-const feature = loadFeature("./features/login-form.feature");
-const expect = require("expect-puppeteer");
-const puppeteer = require("puppeteer");
-
-defineFeature((feature, test) => {
-    
-    beforeEach(async () => {
-        await global.page.goto("http://localhost:3000/#/");
-    });
-
-    // const browser = await puppeteer.launch({
-    //     headless: false,
-    //     defaultViewport: null
-    // });
-
-    test("The user is already registered with a pod and logs in Radarin", ({given,when,then}) => {
->>>>>>> master
     
         let username;
         let pass;
@@ -46,7 +26,6 @@ defineFeature((feature, test) => {
              pass = "Radarintest1.";             
         });
     
-<<<<<<< HEAD
         when('I fill in the user name field and press the login button', async () => {                        
             await page.setViewport({ width: 1400, height: 900 });
             await expect(page).toMatch('Radarin');
@@ -61,21 +40,6 @@ defineFeature((feature, test) => {
             await expect(page).toClick('button', {text: 'Log In'});
             await delay(2000);
             
-=======
-        when("I fill in the user name field and press the login button", async () => {
-            await expect(page).toMatch("Radarin");
-            await expect(page).toFill("input[name='userName']", username);
-            
-            await expect(page).toClick("button", { text: "Log in" }); 
-            
-            await page.goto("https://solidcommunity.net/login");
-            await expect(page).toMatch("Login");
-            await expect(page).toFill("input[name='username']", username);
-            await expect(page).toFill("input[name='password']", pass);
-
-            await expect(page).toClick("button", {text: "Log In"});
-            await page.goto("http://localhost:3000/#/profile");
->>>>>>> master
         });
     
         then("I should enter in my radarin profile", async () => {
--- webapp/e2e/steps/register-form.steps.js
+++ webapp/e2e/steps/register-form.steps.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const {defineFeature, loadFeature}=require('jest-cucumber');
 const feature = loadFeature('./features/register-form.feature');
 const {setDefaultOptions} = require('expect-puppeteer');
@@ -10,15 +9,10 @@ function delay(time) {
       setTimeout(resolve, time)
   });
 }
-=======
-const {defineFeature, loadFeature}=require("jest-cucumber");
-const feature = loadFeature("./features/register-form.feature");
->>>>>>> master
 
 defineFeature((feature, test) => {
   
   beforeEach(async () => {
-<<<<<<< HEAD
     await global.page.goto('http://localhost:3000'),
     setDefaultOptions({ timeout: 10000 });
   });
@@ -91,54 +85,6 @@ defineFeature((feature, test) => {
       await delay(500);
       await expect(page).toMatch("Account already exists");
       await delay(3000);     
-=======
-    await global.page.goto("http://localhost:3000", {timeout: 60000});
-  });
-
-  test("The user is not registered in SOLID", ({given,when,then}) => {
-
-    given("An unregistered user without a POD", () => {});
-
-    when("I click on the Sign up link", async () => {
-      await page.goto("http://localhost:3000/#/register");      
-    });
-
-    then("I should be redirected to http://localhost:3000/#/register", async () => {
-      await expect(page).toMatch("Get your SOLID pod");
-      //If there were someday buttons below here, the world would be a better place...
-      //await expect(page).toClick("button", {text : "Solid Community"});      
-      await page.goto("https://solidcommunity.net/register");
-      await expect(page).toMatch("Already have an account?");
-    });
-  });
-
-  /*
-  test("The user create a non existing Solid POD", ({ given, when, then }) => {
-    
-    let username;
-    let password;
-
-    given("An unregistered user without a POD", () => {
-      username = "teste2e";
-      password = "Teste2een3a.";
-    });
-
-    when("I fill the data in the form and press submit", async () => {
-        await global.page.goto("https://localhost:3000/register", {timeout: 60000});
-        await expect(page).toFillForm("form#RegisterForm", {
-          username,
-          password,
-          repeat_password: password,
-          name: username,
-        });
-        await Promise.all([
-          expect(page).toClick("button#register"),
-          page.waitForNavigation({
-            waitUntil: "networkidle2",
-          })
-        ]);
-        await expect(await page.url()).toBe("https://teste2e.localhost:3000/");
->>>>>>> master
     });
   });
 
@@ -147,7 +93,6 @@ defineFeature((feature, test) => {
     let username;
     let password;
 
-<<<<<<< HEAD
     given('An unregistered user without a POD', () => {
       username = "test321";
       password = "Radarin321.";
@@ -156,9 +101,6 @@ defineFeature((feature, test) => {
     when('I click on the Solid Community at the Sign up link', async () => {      
       await page.setViewport({ width: 1400, height: 900 });
       await expect(page).toMatch('Radarin');
-=======
-    then("I should be redirected to your Solid POD", async () => {
->>>>>>> master
       
       const signup = await select(page).getElement('a:contains(Sign up)');
       await signup.click();
