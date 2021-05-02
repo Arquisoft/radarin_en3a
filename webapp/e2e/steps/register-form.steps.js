1724d1783b62c496a9fb7ad7e5d5c06f634c3671
UO225811 <UO225811@uniovi.es>
2021-05-02 18:37:13 +0200

    Performing register steps

--- webapp/e2e/steps/register-form.steps.js
+++ webapp/e2e/steps/register-form.steps.js
@@ -4,6 +4,12 @@ const {setDefaultOptions} = require('expect-puppeteer');
 const puppeteer = require('puppeteer');
 const select = require ('puppeteer-select');
 
+function delay(time) {
+  return new Promise(function(resolve) { 
+      setTimeout(resolve, time)
+  });
+}
+
 defineFeature(feature, test => {
   
   beforeEach(async () => {
@@ -27,7 +33,7 @@ defineFeature(feature, test => {
       
       const signup = await select(page).getElement('a:contains(Sign up)');
       await signup.click();
-      await delay(1000);
+      await delay(3000);
       await expect(page).toMatch('Get your pod');
       const solid = await select(page).getElement('a:contains(Solid Community)');
       await solid.click();
@@ -43,6 +49,42 @@ defineFeature(feature, test => {
       await expect(page).toClick('button', {text: 'Register'});
       await expect(page).toMatch("https://test321.solidcommunity.net/");
       await delay(3000);
+    });
+  });
+
+  test('The user create an already existing solid POD', ({given,when,then}) => {
+
+    let username;
+    let password;
+
+    given('An unregistered user without a POD', () => {
+      username = "test321";
+      password = "Radarin321.";
+    })
+
+    when('I click on the Solid Community at the Sign up link', async () => {      
+      await page.setViewport({ width: 1400, height: 900 });
+      await expect(page).toMatch('Radarin');
+      
+      const signup = await select(page).getElement('a:contains(Sign up)');
+      await signup.click();
+      await delay(1000);
+      await expect(page).toMatch('Get your pod');
+      const solid = await select(page).getElement('a:contains(Solid Community)');
+      await solid.click();
+      await delay(2000);
+    });
+
+    then('I should be told that the user already exists', async () => {      
+      await expect(page).toFill('input[name="username"]', username);
+      await expect(page).toFill('input[name="password"]', password);
+      await expect(page).toFill('input[name="repeat_password"]', password);
+      await expect(page).toFill('input[name="name"]', "RadarinTest321");
+      await expect(page).toFill('input[name="email"]', "testpurposes@gmail.com");
+      await expect(page).toClick('button', {text: 'Register'});
+      await delay(500);
+      await expect(page).toMatch("Account already exists");
+      await delay(3000);     
     });
   });
 
