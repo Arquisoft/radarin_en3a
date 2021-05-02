7f13463ab5c59d6f5e8aeec21222be1a9b8b10ce
UO225811 <UO225811@uniovi.es>
2021-05-02 17:53:58 +0200

    Fixed all the tests

--- webapp/e2e/features/navigation.feature
+++ webapp/e2e/features/navigation.feature
@@ -5,12 +5,12 @@ Scenario: The user logs in Radarin and navigate to friends view
   When I try to go to different links of the app
   Then I should enter in radarin friends view
 
-# Scenario: The user logs in Radarin and navigate to map view
-#   Given An already registered user in the profile page 
-#   When I try to go to different links of the app
-#   Then I should enter in radarin map view
+Scenario: The user logs in Radarin and navigate to map view
+  Given An already registered user in the profile page 
+  When I try to go to different links of the app
+  Then I should enter in radarin map view
 
-# Scenario: The user logs in Radarin and navigate to locations view
-#   Given An already registered user in the profile page 
-#   When I try to go to different links of the app
-#   Then I should enter in radarin locations view
+Scenario: The user logs in Radarin and navigate to locations view
+  Given An already registered user in the profile page 
+  When I try to go to different links of the app
+  Then I should enter in radarin locations view
--- webapp/e2e/steps/navigation.steps.js
+++ webapp/e2e/steps/navigation.steps.js
@@ -10,11 +10,25 @@ function delay(time) {
     });
  }
 
+ function fakeLocation(latitude, longitude) {
+    return {
+      onBeforeLoad(win) {
+        cy.stub(win.navigator.geolocation, "getCurrentPosition", (cb, err) => {
+          if (latitude && longitude) {
+            return cb({ coords: { latitude, longitude } });
+          }
+          throw err({ code: 1 }); // 1: rejected, 2: unable, 3: timeout
+        });
+      }
+    };
+  }
+
 defineFeature(feature, test => {
     
     beforeEach(async () => {
         await global.page.goto('http://localhost:3000'),
-        setDefaultOptions({ timeout: 10000 })
+        setDefaultOptions({ timeout: 10000 }),
+        fakeLocation(48,2)
     });
 
     test('The user logs in Radarin and navigate to friends view', ({given,when,then}) => {
@@ -48,79 +62,47 @@ defineFeature(feature, test => {
     
         then('I should enter in radarin friends view', async () => {
             await expect(page).toMatch('Friend list');
+            await delay(2000);
         });
     });
     
-    // test('The user logs in Radarin and navigate to map view', ({given,when,then}) => {
+    test('The user logs in Radarin and navigate to map view', ({given,when,then}) => {
+        
+        let long;
+        let lat;
+        
+        given('An already registered user in the profile page', () => {
+            long = 2;
+            lat = 48;
+        });
     
-    //     let username;
-    //     let pass;
+        when('I try to go to different links of the app', async () => {                        
+            await delay(2000);
+            await expect(page).toMatch('Radarin');            
+            const map = await select(page).getElement('a:contains(Map)');
+            await map.click();            
+        });
+    
+        then('I should enter in radarin map view', async () => {
+            await expect(page).toMatch('Map of your locations:');
+        }); 
+    });
 
-    //     given('An already registered user in the profile page', () => {
-    //          username = "radarintest";
-    //          pass = "Radarintest1.";             
-    //     });
+    test('The user logs in Radarin and navigate to locations view', ({given,when,then}) => {
     
-    //     when('I try to go to different links of the app', async () => {                        
-    //         await page.setViewport({ width: 1400, height: 900 });
-    //         /*
-    //         await expect(page).toMatch('Radarin');
-    //         await expect(page).toFill('input[name="userName"]', username);
-    //         await expect(page).toClick('button', {text: 'Log in'});
-    //         await delay(3000);
-            
-    //         await expect(page).toMatch('Login');
-    //         await expect(page).toFill('input[name="username"]', username);
-    //         await expect(page).toFill('input[name="password"]', pass);
-            
-    //         await expect(page).toClick('button', {text: 'Log In'});
-    //         await delay(1000);
-    //         */
-    //         const map = await select(page).getElement('a:contains(Map)');
-    //         await map.click();
-            
-    //         await delay(2000);
-    //     });
-    
-    //     then('I should enter in radarin map view', async () => {
-    //         await expect(page).toMatch('Map of your locations:');
-    //     }); 
-    // });
+       
 
-    // test('The user logs in Radarin and navigate to locations view', ({given,when,then}) => {
+        given('An already registered user in the profile page', () => {});
     
-    //     let username;
-    //     let pass;
-
-    //     given('An already registered user in the profile page', () => {
-    //          username = "radarintest";
-    //          pass = "Radarintest1.";             
-    //     });
+        when('I try to go to different links of the app', async () => {                        
+            await delay(3000);
+            await expect(page).toMatch('Radarin');           
+            const locations = await select(page).getElement('a:contains(Locations)');
+            await locations.click();            
+        });
     
-    //     when('I try to go to different links of the app', async () => {                        
-    //         await page.setViewport({ width: 1400, height: 900 });
-            
-    //         await expect(page).toMatch('Radarin');
-    //         await expect(page).toFill('input[name="userName"]', username);
-    //         await expect(page).toClick('button', {text: 'Log in'});
-    //         await delay(2000);
-    //         /*
-    //         await expect(page).toMatch('Login');
-    //         await expect(page).toFill('input[name="username"]', username);
-    //         await expect(page).toFill('input[name="password"]', pass);
-            
-    //         await expect(page).toClick('button', {text: 'Log In'});
-    //         await delay(2000);
-    //         */
-    //         const [locations] = await page.$x("//a[contains(., 'Locations')]");
-    //         if (locations) {
-    //             await locations.click();
-    //         }
-    //         await delay(3000);
-    //     });
-    
-    //     then('I should enter in radarin locations view', async () => {
-    //         await expect(page).toMatch('Your locations');
-    //     });
-    // });
+        then('I should enter in radarin locations view', async () => {
+            await expect(page).toMatch('Your locations');
+        });
+    });
 });
