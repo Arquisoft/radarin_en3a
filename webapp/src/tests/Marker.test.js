63771f356e1c66073ce83a6356c32dbcf6136092
Sergio <uo270411@uniovi.es>
2021-05-05 20:11:28 +0200

    Improving code cov

--- webapp/src/tests/Marker.test.js
+++ webapp/src/tests/Marker.test.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React from 'react'
 import { render } from "@testing-library/react";
 import MarkerGenerator from "./../components/map/MarkerGenerator";
+import { getSolidDataset, getThing, getUrlAll } from '@inrupt/solid-client';
 
 test('check that everything is rendering propertly', async () => {
-  const locationList = [];
+  const webId = 'https://sergioen3a.solidcommunity.net/profile/card#me';
+
+global.session = jest.fn();
+global.session.info = jest.fn();
+global.session.info.webId = webId;
+
+
+const locationList = [];
+
   const { getByText } = render(<MarkerGenerator locationList={locationList} />);
   expect(getByText("No locations currently saved on your POD")).toBeInTheDocument();
+   
 });  
